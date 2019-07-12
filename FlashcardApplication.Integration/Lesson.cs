b51dd141d0c204f256d411b2d333d3f404cac2e9
Elsanussi Mneina <elsanussimneina@Elsanussis-MacBook-Pro.local>
2019-07-11 20:21:39 -0400

    Refactor to use a list.

--- FlashcardApplication.Integration/Lesson.cs
+++ FlashcardApplication.Integration/Lesson.cs
@@ -1,13 +1,22 @@
+using System.Collections.Generic;
+
 namespace FlashcardApplication.Integration
 {
     public class Lesson
     {
         private const string Flashcard1 = "the | le/la";
         private const string Flashcard2 = "a | un/une";
+        private readonly IList<string> Flashcards = new List<string> { Flashcard1, Flashcard2};
 
         public string LessonSummary()
         {
-            return ShowFlashcard(Flashcard1) + ShowFlashcard(Flashcard2);
+            string result = string.Empty;
+            foreach (string flashcard in Flashcards)
+            {
+                result += ShowFlashcard(flashcard);
+            }
+
+            return result;
         }
 
         public string ShowFlashcard(string flashcard)
