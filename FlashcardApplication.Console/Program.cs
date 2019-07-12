b02c4b4a26a59b280789298ac07440b65995f14f
Elsanussi Mneina <elsanussimneina@Elsanussis-MacBook-Pro.local>
2019-07-11 22:41:31 -0400

    Implement keep prompting for user input.

--- FlashcardApplication.Console/Program.cs
+++ FlashcardApplication.Console/Program.cs
@@ -25,36 +25,44 @@ namespace flashcard_application_cs_oo
             Greetings greetings = new Greetings();
             Console.WriteLine(greetings.WelcomeMessage); // Show a welcome message.
             Console.WriteLine();  // blank line
-            Console.WriteLine("Enter 'a' to show both front and back of each card.");
-            Console.WriteLine("Enter 'f' to show the front of each card.");
-            Console.WriteLine("Enter 'b' to show the back of each card.");
-            Console.WriteLine("Enter 'x' to exit the application.");
-            Console.Write("> "); // terminal prompt to show the user
-            string userInput = Console.ReadLine();
-            Lesson lesson = new Lesson();
-            if (userInput == "a")
+            StartCommandLineLoop();
+        }
+
+        private static void StartCommandLineLoop()
+        {
+            while (true)
             {
-                Console.WriteLine("Printing Lesson summary:");
-                Console.WriteLine(lesson.LessonSummary(Flashcards));
-            }
-            else if (userInput == "f")
-            {
-                Console.WriteLine("Print only fronts of each card:");
-                Console.WriteLine(lesson.FrontSummary(Flashcards));
-            }
-            else if (userInput == "b")
-            {
-                Console.WriteLine("Print only backs of each card:");
-                Console.WriteLine(lesson.BackSummary(Flashcards));
-            }
-            else if (userInput == "x")
-            {
-                Console.WriteLine("Exiting...");
-                System.Environment.Exit(0);
-            }
-            else
-            {
-                Console.WriteLine("Unrecognized input: (" + userInput + ")");
+                Console.WriteLine("Enter 'a' to show both front and back of each card.");
+                Console.WriteLine("Enter 'f' to show the front of each card.");
+                Console.WriteLine("Enter 'b' to show the back of each card.");
+                Console.WriteLine("Enter 'x' to exit the application.");
+                Console.Write("> "); // terminal prompt to show the user
+                string userInput = Console.ReadLine();
+                Lesson lesson = new Lesson();
+                if (userInput == "a")
+                {
+                    Console.WriteLine("Printing Lesson summary:");
+                    Console.WriteLine(lesson.LessonSummary(Flashcards));
+                }
+                else if (userInput == "f")
+                {
+                    Console.WriteLine("Print only fronts of each card:");
+                    Console.WriteLine(lesson.FrontSummary(Flashcards));
+                }
+                else if (userInput == "b")
+                {
+                    Console.WriteLine("Print only backs of each card:");
+                    Console.WriteLine(lesson.BackSummary(Flashcards));
+                }
+                else if (userInput == "x")
+                {
+                    Console.WriteLine("Exiting...");
+                    System.Environment.Exit(0);
+                }
+                else
+                {
+                    Console.WriteLine("Unrecognized input: (" + userInput + ")");
+                }
             }
         }
     }
