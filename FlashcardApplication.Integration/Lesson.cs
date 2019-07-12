f04864dab1a90e4ffa6b854c5ebe85fdd75a3f14
Elsanussi Mneina <elsanussimneina@Elsanussis-MacBook-Pro.local>
2019-07-11 20:05:12 -0400

    Refactor: break LessonSummary into two constants.

--- FlashcardApplication.Integration/Lesson.cs
+++ FlashcardApplication.Integration/Lesson.cs
@@ -2,9 +2,12 @@ namespace FlashcardApplication.Integration
 {
     public class Lesson
     {
+        private const string Flashcard1 = "the | le/la";
+        private const string Flashcard2 = "a | un/une";
+
         public string LessonSummary()
         {
-            return "the | le/la \n a | un/une\n";
+            return Flashcard1 + " \n " + Flashcard2 + "\n";
         }
     }
 }
