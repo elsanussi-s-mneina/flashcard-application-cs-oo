805daa31ced1deee0087745593e44acfbbff5f24
elsanussi-s-mneina <52800671+elsanussi-s-mneina@users.noreply.github.com>
2020-02-21 21:15:26 -0500

    Show multiple flashcards in the web browser (as plain text)
    Actually create instances of the Flashcard class,

--- FlashcardApplication.Web.ASP/Controllers/LessonController.cs
+++ FlashcardApplication.Web.ASP/Controllers/LessonController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using FlashcardApplication.Integration;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,10 +11,17 @@ namespace FlashcardApplication.Web.ASP.Controllers
 {
     public class LessonController : Controller
     {
+        IList<Flashcard> flashcards = new List<Flashcard>
+        {
+            new Flashcard("the", "le/la"),
+            new Flashcard("I", "je"),
+            new Flashcard("we", "nous")
+        };
+
         // GET: /<controller>/
         public string Index()
         {
-            return "the | a";
+            return new Lesson().LessonSummary(flashcards);
         }
     }
 }
