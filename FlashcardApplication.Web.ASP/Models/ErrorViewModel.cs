969328960564c010b56d4e5571d0ee9be136176a
elsanussi-s-mneina <52800671+elsanussi-s-mneina@users.noreply.github.com>
2020-02-21 19:37:39 -0500

    Start ASP.NET project.

---
+++ FlashcardApplication.Web.ASP/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace FlashcardApplication.Web.ASP.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
